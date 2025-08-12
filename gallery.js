dd0d6fc66d2bc70adc4307b72b0caa6888cdc35b
Samlg <samlourdonguerrier@gmail.com>
2025-08-12 15:09:54 -0400

    add tabindex selector capabilities

--- gallery.js
+++ gallery.js
@@ -4,8 +4,9 @@ function upDate(previewPic){
  /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
     to the source file of the preview image
-    
+
    */
+    console.log(previewPic)
    let imageDiv = document.getElementById('image');
     imageDiv.style.backgroundImage = "url('" + previewPic.src + "')";
     imageDiv.innerHTML = previewPic.alt;
@@ -18,3 +19,23 @@ function upDate(previewPic){
     imageDiv.style.backgroundImage = "url('')";
     imageDiv.innerHTML = "Hover over an image below to display here.";
 	}
+
+    // let gallery = ["https://cdn.pixabay.com/photo/2022/10/23/19/10/car-7541935_1280.jpg","https://cdn.pixabay.com/photo/2019/08/09/06/12/car-racing-4394450_1280.jpg","https://cdn.pixabay.com/photo/2023/10/19/17/13/bmw-8327275_1280.jpg",
+    //     "https://s3-us-west-2.amazonaws.com/s.cdpn.io/389177/bacon3.jpg",
+    //     "https://upload.wikimedia.org/wikipedia/commons/e/ec/The_Citadelle_Laferri%C3%A8re%2C_Haiti_%287761638618%29.jpg",
+    //     "https://upload.wikimedia.org/wikipedia/commons/e/ec/The_Citadelle_Laferri%C3%A8re%2C_Haiti_%287761638618%29.jpg"
+    // ]
+    
+    // let images = document.getElementsByTagName('img');
+    // for(i = 0; i < images; i++);
+
+    // images[i].setAttribute("tabindex", "0")
+
+
+    const images = document.querySelectorAll('.preview');
+images.forEach((element,i)=>{
+      this.upDate(element);
+  element.setAttribute("tabindex","0"); 
+
+
+})
